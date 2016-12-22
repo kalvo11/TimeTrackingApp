e49126e4567a7c5a88fbd3998d16387aa98c431b
Kelvin <kelvinschulz@rocketmail.com>
2016-12-22 21:06:40 +0100

    TITR3: Zeit kann nun hinzugefügt werden // Stunden, Minuten und Sekunden werden korrekt ausgerechnet und angezeigt.

--- TimeTracker/MainWindow.xaml.cs
+++ TimeTracker/MainWindow.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace TimeTracker
 {
     public partial class MainWindow : Window
     {
+        double Seconds, Minutes, Hours = 0;
+        DispatcherTimer timer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
@@ -21,6 +25,42 @@ namespace TimeTracker
         private void CloseClick(object sender, RoutedEventArgs e)
         {
             CloseMainWindow();
+        }
+
+        private void btnWorkStartStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (!timer.IsEnabled)
+            {
+                btnWorkStartStop.Content = "Stop";
+                timer.Tick += new EventHandler(timer_Tick);
+                timer.Interval = new TimeSpan(0, 0, 1);
+                timer.Start();
+            }
+            else
+            {
+                btnWorkStartStop.Content = "Start";
+                timer.Stop();
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Seconds++;
+
+            if (Seconds == 60)
+            {
+                Seconds = 0;
+                Minutes++;
+            }
+
+            if (Minutes == 60)
+            {
+                Minutes = 0;
+                Hours++;
+            }
+
+            lblActiveSessionDuration.Content =
+                Hours.ToString() + " Stunden " + Minutes.ToString() + " Minuten " + Seconds.ToString() + " Sekunden";
         }
     }
 }
